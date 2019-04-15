bfe694fbe44df71e861ad5535a8ed6c5a95282f9
oyeu <daarangovi@gmail.com>
2019-04-14 23:34:42 -0500

    cambio

--- src/config/config.js
+++ src/config/config.js
@@ -1,2 +1,12 @@
 process.env.PORT = process.env.PORT || 3000;
-process.env.URLDB = 'mongodb://localhost:27017/asignaturas';
+process.env.NODE_ENV = process.env.NODE_ENV || 'local';
+
+let urlDB
+if (process.env.NODE_ENV === 'local'){
+	urlDB = 'mongodb://localhost:27017/asignaturas';
+}
+else {
+	urlDB = 'mongodb+srv://nodejstdea:nodejstdea@nodejstdea-4jn4i.mongodb.net/asignaturas?retryWrites=true'
+}
+
+process.env.URLDB = urlDB
